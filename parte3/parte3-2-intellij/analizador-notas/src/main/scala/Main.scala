b6e860e6947f7f32e5eb88fdd29bbaeba7fde8b6
Lorenley Rodriguez Rodriguez <lorenleyrodriguez4@gmail.com>
2026-09-21 20:20:01 +0200

    code:Main.scala
    Creación de funciones básicas y declaración de las primeras calificaciones

---
+++ parte3/parte3-2-intellij/analizador-notas/src/main/scala/Main.scala
@@ -0,0 +1,109 @@
+package src.main.scala
+
+object Main extends App {
+  val estudiantes = List(
+    "Ana",
+    "Luis",
+    "Marta",
+    "Pedro",
+    "Sofia"
+  )
+
+  val notas = Array(
+    8,
+    4,
+    10,
+    6,
+    3
+  )
+
+
+  // FUNCIONES
+  def aprobado(nota: Int): Boolean={
+    nota>= 5
+
+  }
+
+  def estadoNota(nota: Int):String= {
+    if (aprobado(nota)){
+      "APROBADO"
+    } else {
+      "SUSPENSO"
+    }
+  }
+
+  def maxNota(a: Int, b: Int): Int = {
+
+    if (a > b) {
+      a
+    } else {
+      b
+    }
+  }
+
+  def mostrarNotas(notasEvaluacion: Array[Int]): Unit = {
+
+    var i = 0
+
+    while (i < notasEvaluacion.length) {
+
+      println(
+        s"${estudiantes(i)} -> ${notasEvaluacion(i)} -> ${estadoNota(notasEvaluacion(i))}"
+      )
+
+      i += 1
+    }
+  }
+
+  println("PRIMERA EVALUACIÓN")
+  mostrarNotas(notas)
+
+  def contarAprobados(notasEvaluacion: Array[Int]): Int = {
+
+    var aprobados = 0
+    var i = 0
+
+    while (i < notasEvaluacion.length) {
+
+      if (aprobado(notasEvaluacion(i))) {
+        aprobados += 1
+      }
+
+      i += 1
+    }
+
+    aprobados
+  }
+
+
+  def mejorNota(notasEvaluacion: Array[Int]): Int = {
+
+    var mejor = 0
+    var i = 0
+
+    while (i < notasEvaluacion.length) {
+
+      mejor = maxNota(mejor, notasEvaluacion(i))
+
+      i += 1
+    }
+
+    mejor
+  }
+
+
+  def mostrarResumen(notasEvaluacion: Array[Int]): Unit = {
+
+    val numeroAprobados = contarAprobados(notasEvaluacion)
+    val numeroSuspensos = notasEvaluacion.length - numeroAprobados
+
+    println()
+    println("--- Resumen del grupo ---")
+    println()
+
+    println(s"Estudiantes: ${estudiantes.length}")
+    println(s"Aprobados: $numeroAprobados")
+    println(s"Suspensos: $numeroSuspensos")
+    println(s"Mejor nota: ${mejorNota(notasEvaluacion)}")
+  }
+}
